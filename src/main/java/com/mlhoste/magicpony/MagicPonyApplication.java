cb177792a2cf7105b5fb0a4766410fa32ab4c08b
Mickaël LHOSTE <mickael.lhoste@gmail.com>
2017-05-30 15:43:05 +0200

    dev: project architecture draft

---
+++ src/main/java/com/mlhoste/magicpony/MagicPonyApplication.java
@@ -0,0 +1,12 @@
+package com.mlhoste.magicpony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicPonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicPonyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mlhoste/magicpony/MagicPonyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mlhoste.magicpony;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MagicPonyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
