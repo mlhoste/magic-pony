690ed4ebb8b0d9b280503ccc2a7e3db8548352e7
Mickaël LHOSTE <mickael.lhoste@gmail.com>
2017-06-01 17:46:02 +0200

    dev: first main MagicPonyApplication

--- src/main/java/com/mlhoste/magicpony/MagicPonyApplication.java
+++ src/main/java/com/mlhoste/magicpony/MagicPonyApplication.java
@@ -1,12 +1,16 @@
 package com.mlhoste.magicpony;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MagicPonyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MagicPonyApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(MagicPonyApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicPonyApplication.class, args);
+    }
 }
